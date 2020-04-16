2438d947635bd219af30b57202443c84660f4024
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-17 01:40:02 +0200

    Add files via upload

---
+++ PT - PHP/TP5 PHP/Exe2.php
@@ -0,0 +1,14 @@
+<?php 
+   define("Pi", 3.14 );
+     $rayon=5;
+   $surface=Pi*$rayon;
+   echo($surface);
+?>
+<hr>
+<?php 
+   define("anne", 2020 );
+     $nom="Taib";
+	 $anneeNaissance​ = 1999;
+   $age= anne -$anneeNaissance​;
+   echo("Bonjour ". $nom ." vous avez ".$age." ans");
+?>
