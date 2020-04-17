3615adce10312274a5f88a2d942f853cb18aa95f
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-17 03:34:46 +0200

    Update Exe1.php

--- PT - PHP/TP5 PHP/Exe1.php
+++ PT - PHP/TP5 PHP/Exe1.php
@@ -1,9 +1,30 @@
-//Exercice1 :
+//Exercice1 :---------------------------------------------------------------------
 <?php
      $dty = date('d/m/Y');
 	 $dts = date('h:i:s');
 	 print("Nous sommes le :" . $dty . "<br>il est " . $dts . "<br>Merci");
 ?>
 <br>
-//Exercice2 : 
+//Exercice2 :---------------------------------------------------------------------
+<?php
+     function PHP($tab){
+		 for($a=0;$a<=count($tab);$a++){
+			 if($tab[$a]<0){
+				 $x++;
+			 }else{
+				 $y++;
+			 }
+		 }
+		 if($x=$y){
+			 $k = 0;
+		 }else{
+			 if($x<$y){
+				 $k =  1;
+			 }else{
+				 $k = -1;
+			 }
+		 }
+		 return $k;
+	 }
+?>
 
