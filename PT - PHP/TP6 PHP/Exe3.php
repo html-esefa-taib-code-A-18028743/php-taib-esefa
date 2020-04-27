51407079eca6d49ebf12853120086fa2d3a537f6
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-28 00:39:45 +0200

    Update Exe3.php

--- PT - PHP/TP6 PHP/Exe3.php
+++ PT - PHP/TP6 PHP/Exe3.php
@@ -1,13 +1,3 @@
 <?php
-    $prix_table=150;
-	$prix_armoire=50;
-	$nombre=10;
-	$x=$prix_armoire*$nombre;
-	// 2 :
-	echo("le prix plus élevé : ");
-	if($prix_armoire < $prix_table){
-		echo($prix_table);
-	}else{
-		echo($prix_armoire);
-	}
+
 ?>
