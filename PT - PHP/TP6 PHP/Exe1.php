1a3ddf44f4dc58c4a29424096045aaf017d95ace
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-28 00:36:30 +0200

    Update Exe1.php

--- PT - PHP/TP6 PHP/Exe1.php
+++ PT - PHP/TP6 PHP/Exe1.php
@@ -1,4 +1,17 @@
 function connexion(){
-    $base = mysql_connect ('localhost', 'root', '');  
-    mysql_select_db ('DB_MGF', $base) ;
+    $link=mysql_connect("localhost", "login","") or die(mysql_error());
+    mysql_select_db("DB_MGF",$link) or die(mysql_error());
+    $log=$_POST[’nom’];$password=$_POST[’pass’];
+    $resultat=mysql_query("SELECT * FROM utilisateurs WHERE login=’$log’");
+    if (mysql_affected_rows($link)==0)
+    echo "mauvais login<br />";
+    else{
+    $ligne=mysql_fetch_assoc($resultat);
+    if ($_POST[’pass’]==$ligne[’password’]){
+    $_SESSION[’login’]=$_POST["nom"];
+    header("location : ".$_SERVER[’PHP_SELF’]);
+    echo "Bonjour, monsieur ".$_POST["nom"];
 }
+}
+}
+
