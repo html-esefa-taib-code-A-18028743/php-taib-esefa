d8cbc2df1460600fdf882eb75077281ed6064981
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-28 02:34:58 +0200

    Update Exe4.php

--- PT - PHP/TP6 PHP/Exe4.php
+++ PT - PHP/TP6 PHP/Exe4.php
@@ -1,3 +1,24 @@
-<?php
-		 
-?>
+function ajout_commande($id_commande,$id_produit,$id_client,$quantite){
+
+   //Si le panier existe
+   if (creationPanier() && !isVerrouille())
+   {
+      //Si le produit existe déjà on ajoute seulement la quantité
+      $positionProduit = array_search($id_commande,  $_SESSION['panier']['id_commande']);
+
+      if ($positionProduit !== false)
+      {
+         $_SESSION['panier']['quantite'][$positionProduit] += $quantite ;
+      }
+      else
+      {
+         //Sinon on ajoute le produit
+         array_push( $_SESSION['panier']['id_commande'],$id_commande);
+         array_push( $_SESSION['panier']['quantite'],$quantite);
+         array_push( $_SESSION['panier']['id_client'],$id_client);
+         array_push( $_SESSION['panier']['id_produit'],$id_produit);
+      }
+   }
+   else
+   echo "Un problème est survenu veuillez contacter l'administrateur du site.";
+}
